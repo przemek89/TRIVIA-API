79fc6b9352acad067c00b940a464e94141652b74
Przemek Żbikowski <you@example.com>
2020-03-25 09:23:25 +0100

    set Access-Control-Allow

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -15,9 +15,11 @@ def create_app(test_config=None):
 
   CORS(app, resources={r"/*": {origins: '*'}})
 
-  '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS')
+    return response
 
   '''
   @TODO: 
