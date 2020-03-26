a46b51bf80316b189f10561fc710965f4b62bd84
Przemek Żbikowski <you@example.com>
2020-03-26 09:13:57 +0100

    implemented 404, 405 and 422 error handling for all endpoints

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -27,8 +27,11 @@ def create_app(test_config=None):
     categories = {}
     for category in categories_query:
       categories[category.id] = category.type
-    return jsonify({
-      'categories': categories
+    if categories_query is None:
+      abort(404)
+    else:
+      return jsonify({
+        'categories': categories
       })
 
   @app.route('/questions', methods=['GET'])
@@ -48,13 +51,15 @@ def create_app(test_config=None):
       categories[category.id] = category.type
 
     currentCategory = None # I had a problem to understand where should I take it from, and what is it used for, I took the solution from https://knowledge.udacity.com/questions/82424
-
-    return jsonify({
-      'questions': questions[begin_index:end_index],
-      'totalQuestions': totalQuestions,
-      'currentCategory': currentCategory,
-      'categories': categories
-    })
+    if query_questions is None or categories_query is None:
+      abort(404)
+    else:
+      return jsonify({
+        'questions': questions[begin_index:end_index],
+        'totalQuestions': totalQuestions,
+        'currentCategory': currentCategory,
+        'categories': categories
+      })
 
   @app.route('/questions/<int:question_id>', methods=['DELETE'])
   def delete_question(question_id):
@@ -65,6 +70,8 @@ def create_app(test_config=None):
       db.session.rollback()
       if question_to_be_deleted is None:
         abort(404)
+      else:
+        abort(422)
     finally:
       db.session.close()
     return jsonify({
@@ -75,15 +82,19 @@ def create_app(test_config=None):
   @app.route('/questions', methods=['POST'])
   def create_question():
     try:
-      body = request.get_json()
-      question = body.get('question')
-      answer = body.get('answer')
-      category = body.get('category')
-      difficulty = body.get('difficulty')
-      question_to_be_created = Question(question=question, answer=answer, category=category, difficulty=difficulty)
-      question_to_be_created.insert()
+      if request.method == 'PUT':
+        abort(405)
+      else:
+        body = request.get_json()
+        question = body.get('question')
+        answer = body.get('answer')
+        category = body.get('category')
+        difficulty = body.get('difficulty')
+        question_to_be_created = Question(question=question, answer=answer, category=category, difficulty=difficulty)
+        question_to_be_created.insert()
     except:
       db.session.rollback()
+      abort(422)
     finally:
       db.session.close()
     return jsonify({
@@ -96,38 +107,68 @@ def create_app(test_config=None):
     search_term = body.get('searchTerm', '')
     search_result = Question.query.filter(Question.question.ilike(f'%{search_term}%')).all()
     questions = [question.format() for question in search_result]
-    return jsonify({
-      'questions': questions
-    })
+    if search_term is None:
+      abort(404)
+    else:
+      return jsonify({
+        'questions': questions
+      })
 
   @app.route('/categories/<int:category_id>/questions', methods=['GET'])
   def get_question_for_category(category_id):
     requested_category = Category.query.get(category_id)
     questions_query = Question.query.filter(Question.category == requested_category).all()
     questions = [question.format() for question in questions_query]
-    return jsonify({
-      'questions': questions,
-      'totalQuestions': len(questions),
-      'currentCategory': requested_category.type
-    })
+    if requested_category is None:
+      abort(404)
+    else:
+      return jsonify({
+        'questions': questions,
+        'totalQuestions': len(questions),
+        'currentCategory': requested_category.type
+      })
 
-  @app.route('/quizzes', methods=['POST'])
+  @app.route('/quizzes', methods=['POST']) # implement 404, 422
   def get_questions():
-    body = request.get_json()
-    category = body.get('quiz_category')
-    previous_question = body.get('previous_questions')
-    query_questions = Question.query.filter(Question.category == category).filter(Question.id not in previous_question).all()
-    questions = [question.format() for question in query_questions]
-    random_index = random.randint(1, len(questions))
+    try:
+      body = request.get_json()
+      category = body.get('quiz_category')
+      previous_question = body.get('previous_questions')
+      query_questions = Question.query.filter(Question.category == category).filter(Question.id not in previous_question).all()
+      if query_questions is None:
+        abort(404)
+      else:
+        questions = [question.format() for question in query_questions]
+        random_index = random.randint(1, len(questions))
+    except:
+      abort(422)
     return jsonify({
       'questions': questions[random_index]
     })
 
-  '''
-  @TODO: 
-  Create error handlers for all expected errors 
-  including 404 and 422. 
-  '''
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+      'success': False,
+      'error': 404,
+      'message': 'resource not found',
+    }), 404
+
+  @app.errorhandler(405)
+  def not_found(error):
+    return jsonify({
+      'success': False,
+      'error': 405,
+      'message': 'method not allowed',
+    }), 405
+
+  @app.errorhandler(422)
+  def not_found(error):
+    return jsonify({
+      'success': False,
+      'error': 422,
+      'message': 'unprocessable',
+    }), 422
   
   return app
 
